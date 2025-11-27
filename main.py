ec7d18bcf0c8e08ba0e27a46671e8331c8be68e1
ekate <ekaterina.kostrykina2006@gmail.com>
2025-11-27 06:53:34 +0300

    created new branch

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-from lab_python_oop.rectangle import Rectangle
-from lab_python_oop.circle import Circle
-from lab_python_oop.square import Square
-
-from PIL import Image, ImageDraw, ImageFont
-
-
 def main():
-    N = 13
-
-    r = Rectangle(N, N, "синий")
-    c = Circle(N, "зеленый")
-    s = Square(N)
-    s._color = "красный"
-
-    print(r)
-    print(c)
-    print(s)
-
-    img = Image.new('RGB', (500, 400), color='white')
-    draw = ImageDraw.Draw(img)
-    draw.rectangle([50, 50, 200, 150], fill='blue')
-    draw.ellipse([50, 200, 120, 270], fill="green")
-    draw.rectangle([250, 50, 350, 150], fill='red')
-
-    img.save('shapes.png')
-    img.show()
+    return 0
 
 
 if __name__ == "__main__":
